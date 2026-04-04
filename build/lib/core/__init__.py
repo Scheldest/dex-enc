b306f580df28fe67fba74d9a0d400f76a2843289
Scheldest <dedetxd00@gmail.com>
2026-04-04 09:47:30 +0700

    Delete build/lib directory

--- build/lib/core/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import class utama agar bisa diakses langsung dari package core
-from .crypter import DexCrypter
-from .stego import DexStego
-
-# Menentukan apa yang bisa diakses saat menggunakan 'from core import *'
-__all__ = ['DexCrypter', 'DexStego']
